01adf61a626a6c6c633fd21fcee0f7dc41974919
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2022-02-22 14:40:15 +0100

    fix : adjust name

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 compile:
-	gcc -O3 cipress.c -o cipress
+	gcc -O3 cypress.c -o cypress
 test_compression:
 	echo "cccciiiiaaaaoooo" > test_file
-	./cipress -c test_file
+	./cypress -c test_file
 	cat test_file.cpr
 test_extract:
 	rm test_file
-	./cipress -x test_file.cpr
+	./cypress -x test_file.cpr
 	cat test_file
 clean_test:
 	rm *.cpr
--- cypress.c
+++ cypress.c
@@ -3,8 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 
-int compress(char *filename);
-int extract(char *filename);
 int compress_longest_seq(char *filename);
 
 int main(int argc, char **argv)
@@ -19,7 +17,7 @@ int main(int argc, char **argv)
             compress_longest_seq(optarg);
             break;
         case 'x': // extract
-            // extract(optarg);
+            extract_longest_seq(optarg);
             break;
         default:
             // nothing to do
@@ -40,10 +38,6 @@ which appears the less times*/
 int compress_longest_seq(char *filename)
 {
     FILE *src, *dest;
-    int sparse_bit;
-    char c, c_old = '\0';
-    u_int32_t streak_counter = 1;
-    int first_time = 0;
     u_int64_t longest_sequences[Ls] = {0}; // this might become cumbersome
     u_int64_t shortest_sequence[Ss] = {0}; // this too
     u_int64_t max = 0, min = __UINT64_MAX__;
@@ -131,81 +125,12 @@ int compress_longest_seq(char *filename)
 
     return 0;
 }
-
-int compress_rle(char *filename)
+int extract_longest_seq(char *filename)
 {
+
     FILE *src, *dest;
-    int sparse_bit;
-    char c, c_old = '\0';
-    u_int32_t streak_counter = 1;
-    int first_time = 0;
-
-    src = fopen(filename, "r");
-    dest = fopen(strcat(filename, ".cpr"), "wb");
-
-    if (!src || !dest)
-    {
-        printf("Error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((c = fgetc(src)) != -1)
-    {
-
-        if (!first_time)
-        {
-            first_time = 1;
-            c_old = c;
-            continue;
-        }
-        if (c == c_old)
-        {
-            streak_counter++;
-        }
-        else
-        {
-            if (streak_counter > 3)
-            {
-                /*must repeat at least 3 times*/
-                fprintf(dest, "|%u%c", streak_counter, c_old);
-            }
-            else
-            {
-                /*else, dont encode but just store them in compressed file*/
-                for (size_t i = 0; i < streak_counter; i++)
-                {
-                    fprintf(dest, "%c", c_old);
-                }
-            }
-            c_old = c;
-            streak_counter = 1;
-        }
-    }
-    if (streak_counter > 3)
-    {
-        /*must repeat at least 3 times*/
-        fprintf(dest, "|%u%c", streak_counter, c_old);
-    }
-    else
-    {
-        /*else, dont encode but just store them in compressed file*/
-        for (size_t i = 0; i < streak_counter; i++)
-        {
-            fprintf(dest, "%c", c_old);
-        }
-    }
-    fclose(src);
-    fclose(dest);
-
-    return 0;
-}
-
-int extract_rle(char *filename)
-{
-    FILE *src, *dest;
-    int sparse_bit;
-    char c, c_old = 0;
-    u_int32_t streak_counter = 0;
+    u_int16_t longest_sequence;
+    u_int8_t shortest_sequence;
 
     src = fopen(filename, "r");
     filename[strlen(filename) - 4] = '\0';
@@ -216,24 +141,4 @@ int extract_rle(char *filename)
         printf("Error\n");
         exit(EXIT_FAILURE);
     }
-
-    while ((c = fgetc(src)) != -1)
-    {
-        if (c == '|')
-        {
-            fscanf(src, "%u%c", &streak_counter, &c);
-            for (size_t i = 0; i < streak_counter; i++)
-            {
-                fputc(c, dest);
-            }
-        }
-        else
-        {
-            fputc(c, dest);
-        }
-    }
-    fclose(src);
-    fclose(dest);
-
-    return 0;
 }
